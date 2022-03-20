07e7e8003d606247f46908ccd6469f0306cee3f5
k6ymaker <k6ymaker@gmail.com>
2022-03-20 11:43:29 +0800

    add hello-jar

---
+++ hello-jar/src/main/java/com/test/A.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class A {
+    public String a(){
+        return "a";
+    }
+}
---
+++ hello-jar/src/main/java/com/test/Test.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class Test {
+    public static void main(String[] args) {
+        for(String arg:args) {
+            System.out.println(arg);
+        }
+    }
+}
