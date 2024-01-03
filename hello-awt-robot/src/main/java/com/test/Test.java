e4a4a8e836eb33bdd6886ee265e7eed799e26ab1
k6ymaker <k6ymaker@gmail.com>
2024-01-03 19:23:12 +0800

    feat:add awt-robot

---
+++ hello-awt-robot/src/main/java/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+
+public class Test {
+    public static void main(String[] args) throws Exception{
+        Robot robot = new Robot();
+
+        robot.mouseMove(1,1);
+    }
+}
