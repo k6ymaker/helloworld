4ea919df56dcf901a52f08f3e1ffcb19c01bd6b9
k6ymaker <k6ymaker@gmail.com>
2024-01-03 19:26:12 +0800

    feat:add awt-robot

--- hello-awt-robot/src/main/java/com/test/Test.java
+++ hello-awt-robot/src/main/java/com/test/Test.java
@@ -6,7 +6,21 @@ import java.awt.event.InputEvent;
 public class Test {
     public static void main(String[] args) throws Exception{
         Robot robot = new Robot();
-
-        robot.mouseMove(1,1);
+//        double r = 300d;
+//        double x = 600d,y = 600d;
+//        double start = 0d;
+//        while(true){
+//            robot.mouseMove((int)(r*Math.cos(start)+x),(int)(r*Math.sin(start)+y));
+//            robot.delay(50);
+//            start += 0.1d;
+//            if(start>100)
+//                break;
+//        }
+        //robot.mouseMove(480,408);
+        robot.mouseMove(495-10,554+52-26-20);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mousePress(InputEvent.BUTTON2_DOWN_MASK);
+//        robot.mousePress(InputEvent.BUTTON2_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
     }
 }
