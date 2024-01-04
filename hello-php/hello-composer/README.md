5fe2b325e7c3bcbc7118b3bf37ddd196b84b439f
k6ymaker <k6ymaker@gmail.com>
2024-01-04 17:19:02 +0800

    feat:add php

---
+++ hello-php/hello-composer/README.md
@@ -0,0 +1,5 @@
+https://www.runoob.com/w3cnote/composer-install-and-usage.html
+https://docs.phpcomposer.com/03-cli.html
+
+
+composer show --installed
---
+++ hello-php/hello-composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'dd24ea48a351431029cecd6b320a429a0900dd95',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'dd24ea48a351431029cecd6b320a429a0900dd95',
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'reference' => 'd16496318c3e08e3bccfc3866e104e49cf25488a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
