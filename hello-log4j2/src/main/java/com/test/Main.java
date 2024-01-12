ad6f2cc9ef97c1e551596e8d3ba745e1e7f9e158
k6ymaker <k6ymaker@gmail.com>
2024-01-12 14:43:00 +0800

    feat:add log4j2

--- hello-log4j2/src/main/java/com/test/Main.java
+++ hello-log4j2/src/main/java/com/test/Main.java
@@ -6,7 +6,12 @@ import org.slf4j.LoggerFactory;
 public class Main {
     static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        logger.error("{}","hello");
+    public static void main(String[] args)throws Exception {
+        while(true){
+            Thread.sleep(1000 * 1);
+            logger.info("{}","hello");
+            logger.error("{}","hello");
+            logger.warn("{}","hello");
+        }
     }
 }
